43187d1533da31ae8898795a6602ab60b60dac7c
Maxime Peloquin <mpeloquin@landr.com>
2023-12-21 15:02:18 -0500

    test

--- .github/actions/waiting-for-dependencies-action/index.js
+++ .github/actions/waiting-for-dependencies-action/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const os = require("os");
 
 function searchForVersion(rootFolder, depth = 0) {
     if (depth >= 3) {
@@ -44,9 +45,133 @@ function checkDependencies(dependencies, filePath) {
     }
 }
 
+function escapeData(s) {
+    return toCommandValue(s)
+        .replace(/%/g, '%25')
+        .replace(/\r/g, '%0D')
+        .replace(/\n/g, '%0A')
+}
+
+function escapeProperty(s) {
+    return toCommandValue(s)
+        .replace(/%/g, '%25')
+        .replace(/\r/g, '%0D')
+        .replace(/\n/g, '%0A')
+        .replace(/:/g, '%3A')
+        .replace(/,/g, '%2C')
+}
+const CMD_STRING = '::'
+
+class Command {
+    command
+    message
+    properties
+
+    constructor(command, properties, message) {
+        if (!command) {
+            command = 'missing.command'
+        }
+
+        this.command = command
+        this.properties = properties
+        this.message = message
+    }
+
+    toString() {
+        let cmdStr = CMD_STRING + this.command
+
+        if (this.properties && Object.keys(this.properties).length > 0) {
+            cmdStr += ' '
+            let first = true
+            for (const key in this.properties) {
+                if (this.properties.hasOwnProperty(key)) {
+                    const val = this.properties[key]
+                    if (val) {
+                        if (first) {
+                            first = false
+                        } else {
+                            cmdStr += ','
+                        }
+
+                        cmdStr += `${key}=${escapeProperty(val)}`
+                    }
+                }
+            }
+        }
+
+        cmdStr += `${CMD_STRING}${escapeData(this.message)}`
+        return cmdStr
+    }
+}
+export function prepareKeyValueMessage(key, value) {
+    const delimiter = `ghadelimiter_${123}`
+    const convertedValue = toCommandValue(value)
+
+    // These should realistically never happen, but just in case someone finds a
+    // way to exploit uuid generation let's not allow keys or values that contain
+    // the delimiter.
+    if (key.includes(delimiter)) {
+        throw new Error(
+            `Unexpected input: name should not contain the delimiter "${delimiter}"`
+        )
+    }
+
+    if (convertedValue.includes(delimiter)) {
+        throw new Error(
+            `Unexpected input: value should not contain the delimiter "${delimiter}"`
+        )
+    }
+
+    return `${key}<<${delimiter}${os.EOL}${convertedValue}${os.EOL}${delimiter}`
+}
+
+
+export function toCommandValue(input) {
+    if (input === null || input === undefined) {
+        return ''
+    } else if (typeof input === 'string' || input instanceof String) {
+        return input
+    }
+    return JSON.stringify(input)
+}
+
 function setOutput(key, value) {
-    const output = process.env['GITHUB_OUTPUT']
-    fs.appendFileSync(output, `time=hello`)
+    const filePath = process.env['GITHUB_OUTPUT'] || ''
+    console.log(filePath)
+
+    if (filePath) {
+        return issueFileCommand('OUTPUT', prepareKeyValueMessage(name, value))
+    }
+
+    console.log("not filepath")
+
+    process.stdout.write(os.EOL)
+    issueCommand('set-output', {name}, toCommandValue(value))
+}
+
+export function issueFileCommand(command, message) {
+    const filePath = process.env[`GITHUB_${command}`]
+    if (!filePath) {
+        throw new Error(
+            `Unable to find environment variable for file command ${command}`
+        )
+    }
+    if (!fs.existsSync(filePath)) {
+        throw new Error(`Missing file at path: ${filePath}`)
+    }
+
+    fs.appendFileSync(filePath, `${toCommandValue(message)}${os.EOL}`, {
+        encoding: 'utf8'
+    })
+}
+
+export function issueCommand(
+    command,
+    properties,
+    message
+) {
+    const cmd = new Command(command, properties, message)
+    process.stdout.write(cmd.toString() + os.EOL)
 }
 
 searchForVersion('./');
