ae6bb3211b3a1092a2623b8af7246b6e720f64dd
Maxime Peloquin <mpeloquin@landr.com>
2023-12-21 14:33:08 -0500

    test

--- .github/actions/waiting-for-dependencies-action/index.js
+++ .github/actions/waiting-for-dependencies-action/index.js
@@ -1,37 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
-// Function to recursively search for dependencies with a specific version
-function searchForVersion(rootFolder, version, depth = 0) {
+function searchForVersion(rootFolder, depth = 0) {
     if (depth >= 3) {
         return;
     }
-    // Get the list of files in the current folder
     const files = fs.readdirSync(rootFolder);
 
-    // Iterate through each file
     files.forEach((file) => {
         if (file === '.git' || file === '.github') {
             return;
         }
-        // Get the full path of the current file
         const filePath = path.join(rootFolder, file);
-        console.log(depth, file, filePath);
-        // Check if the current file is a directory
+
         if (fs.statSync(filePath).isDirectory()) {
             // If it is a directory, recursively call the function on that directory
-            searchForVersion(filePath, version, depth + 1);
+            searchForVersion(filePath, depth + 1);
         } else if (file === 'package.json') {
-            // If it is a package.json file, read its content
             const content = fs.readFileSync(filePath, 'utf-8');
 
             try {
-                // Parse the JSON content
                 const packageJson = JSON.parse(content);
 
-                // Check if dependencies or devDependencies have the specified version
-                checkDependencies(packageJson.dependencies, version, filePath);
-                checkDependencies(packageJson.devDependencies, version, filePath);
+                checkDependencies(packageJson.dependencies, filePath);
+                checkDependencies(packageJson.devDependencies, filePath);
             } catch (error) {
                 console.error(`Error parsing ${filePath}: ${error.message}`);
             }
@@ -40,23 +32,24 @@ function searchForVersion(rootFolder, version, depth = 0) {
 }
 
 // Function to check dependencies for a specific version
-function checkDependencies(dependencies, version, filePath) {
-    console.log(dependencies, filePath);
+function checkDependencies(dependencies, filePath) {
     if (dependencies) {
         Object.keys(dependencies).forEach((dependency) => {
             const depVersion = dependencies[dependency];
-            if (depVersion === version) {
+            console.log(depVersion);
+            if (depVersion === "1.1.0") {
                 console.log(`Found ${dependency}@${version} in ${filePath}`);
             }
         });
     }
 }
 
-// Specify the root folder to start the search
-const rootFolder = './';
+function setOutput(key, value) {
+    const output = process.env['GITHUB_OUTPUT']
+    console.log(`::set-output name=${key}::${value}`)
+    console.log(output)
+    fs.appendFileSync(output, `${key}=${value}`)
+}
 
-// Specify the version to search for
-const targetVersion = '1.0.0';
-
-// Start the search
-searchForVersion(rootFolder, targetVersion);
+searchForVersion('./');
+setOutput('time', 'true');
